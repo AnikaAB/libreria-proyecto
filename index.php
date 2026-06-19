691a56fd5d92775385ca8b9f2aed8f1169568a98
CORPASEGO <anavasquezmisho@gmail.com>
2026-06-19 12:03:47 -0500

    Create  index.php

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+echo "<h1>📚 Sistema de Librería</h1>";
+echo "<ul>";
+echo "<li><a href='login/login.php'>🔐 Login</a></li>";
+echo "<li><a href='pagos/pagos.php'>💳 Pagos</a></li>";
+echo "<li><a href='reportes/reportes.php'>📊 Reportes</a></li>";
+echo "</ul>";
