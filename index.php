6c7eca57faa04c4ff5b662aa12180eecf79e271c
CORPASEGO <anavasquezmisho@gmail.com>
2026-06-19 13:40:41 -0500

    Update login and reports links in index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 echo "<h1>📚 Sistema de Librería</h1>";
 echo "<ul>";
-echo "<li><a href='/acceso/login.php'>🔐 Login</a></li>";
+echo "<li><a href='/login/login.php'>🔐 Login</a></li>";
 echo "<li><a href='/pagos/pagos.php'>💳 Pagos</a></li>";
-echo "<li><a href='/informes/reportes.php'>📊 Reportes</a></li>";
+echo "<li><a href='/reportes/reportes.php'>📊 Reportes</a></li>";
 echo "</ul>";
